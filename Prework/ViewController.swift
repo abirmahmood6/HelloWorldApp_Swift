ce1e2a9941727180e40de811d039bbf157ee6511
Abir M <102193884+abirmahmood6@users.noreply.github.com>
2022-07-16 21:54:04 -0400

    Label and buttons

--- Prework/ViewController.swift
+++ Prework/ViewController.swift
@@ -13,12 +13,26 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
+    @IBOutlet weak var TextBackground: UIView!
+   
     @IBOutlet weak var TextLabel: UILabel!
+    
     @IBAction func ButtonClicked(_ sender: Any) {
         print("hello")
         TextLabel.textColor = UIColor.orange
-    }
+        }
+    
+    @IBAction func ChangeText(_ sender: Any) {
+        TextLabel.text = "Goodbye 👋"
+        }
+    
+    @IBAction func Button2(_ sender: Any) {
+    TextBackground.backgroundColor = UIColor.orange
+        }
+    
+   
     
 }
+
 
